de19928b62a4d6e0b8028c5153a5e4521dd5ed11
jouky2 <jouke@tayami.net>
2023-11-09 18:06:06 +0100

    update

--- Assets/Scripts/generation.gd
+++ Assets/Scripts/generation.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
-@onready var oldend = $level/end.position
-@onready var newstart = oldend + Vector3(oldend.x, oldend.y, oldend.z + 2)
+@onready var oldend = $level/end.position + Vector3(0, 0, 1)
 @onready var levels = [preload("res://Assets/Prefabs/level1.tscn")]
 var delt = 2
 var rand = RandomNumberGenerator.new()
@@ -16,11 +15,11 @@ func choose_level():
 #	var num = rand.randi() % 1
 	var num = 0
 	var lev = levels[num].instantiate()
-	lev.position = newstart
+	lev.position = oldend
 	add_child(lev)
 	print("new lev!")
-	oldend = lev.get_node("level").position
-	newstart = oldend + Vector3(oldend.x, oldend.y, oldend.z + 2)
+	oldend = lev.get_node("end").position
+	oldend += Vector3(0, 0, 3)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
