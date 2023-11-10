8bbc79b31f372fc170505f88407f9c42b95814fc
jouky2 <jouke@tayami.net>
2023-11-10 17:07:24 +0100

    fixed it!

--- Assets/Scripts/generation.gd
+++ Assets/Scripts/generation.gd
@@ -15,15 +15,15 @@ func choose_level():
 #	var num = rand.randi() % 1
 	var num = 0
 	var lev = levels[num].instantiate()
+	oldend += Vector3(0, 0, lev.beginadd)
 	lev.position = oldend
 	add_child(lev)
 	print("new lev!")
-	oldend = lev.get_node("end").position
-	oldend += Vector3(0, 0, 3)
+	oldend += Vector3(0, 0, lev.endadd)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	delt += delta
-	if delt > 3:
+	if delt > 1.5:
 		delt = 0
 		choose_level()
